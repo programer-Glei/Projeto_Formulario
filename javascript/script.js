6a8776da4ea824422c8ed894ffe5e79629630280
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-03-19 23:43:06 -0300

    proximo comit

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,42 @@ let validador = {
     handleSubmit:(Event)=>{
         Event.preventDefault();
 
-        
+        let send = true;
+
+        let imput = document.querySelectorAll('input');
+
+        for(let i=0; i<imput.length; i++){
+            let input = imput[i];
+            let checar = validador.checarInput(input);
+            if(checar !== true){
+                send = false
+            }
+        }
+        send = false;
+        if(send){
+            form.submit();
+        }
+    },
+
+    checarInput: (input) =>{
+        let rules = input.getAttribute('data-rules');
+        if(rules !== null){
+            rules = rules.split('|');
+            for(let k in rules){
+                let rDetails = rules[k].split('=');
+                switch(rDetails[0]){
+                    case 'rerquired':
+                        if(input.value == ''){
+                            return 'Campo não pode ser vazio';
+                        }
+                    break;
+                    case 'min':
+                        
+                }
+            }
+        }
+
+        return true;
     }
 }
 
